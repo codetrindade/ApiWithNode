453391a0b36ca11dd2631f0f5e38135eb2a7a4fe
Trindade Code <trindadecode@gmail.com>
2023-06-06 20:11:06 +0100

    moved to bin

--- bin/server.js
+++ bin/server.js
@@ -1,24 +1,14 @@
 'use strict';
+const app = require("../src/app");
 const http = require('http');
-const debug = require('debug')('nodestr:server');
-const express = require('express');
+//const debug = require('debug')('nodestr:server');
 
-const app = express();
 const port = normalizePort(process.env.port || 3000);
 app.set('port', port);
 const server = http.createServer(app);
-const router = express.Router();
-
-const route = router.get('/', (req, res, next)=>{
-    res.status(200).send({
-        "title": "Estudo de API",
-        "version": "0.0.1"
-    });
-} );
-
-app.use("/", route);
 server.listen(port);
 server.on('error',port);
+
 function normalizePort(val){
     const port = parseInt(val, 10);
     if(isNaN(port)){
@@ -29,7 +19,6 @@ function normalizePort(val){
     }
     return false;
 }
-
 function onError(error){
     if(error.syscall !== 'listen'){
         throw error;
@@ -48,5 +37,4 @@ function onError(error){
             throw error; 
     }
 }
-
 console.log("API HAS BEEN STARTED: " + port);
