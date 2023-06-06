f0bdf2c67b6c603fa2db974ac93eae146faa7b0f
Trindade Code <trindadecode@gmail.com>
2023-06-07 00:44:56 +0100

    body parser

--- src/app.js
+++ src/app.js
@@ -1,14 +1,33 @@
 'use strict';
 
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
+app.use(bodyParser);
+app.use(bodyParser.urlencoded({extended: false}));
+
 const router = express.Router();
+
+
 const route = router.get('/', (req, res, next)=>{
     res.status(200).send({
         "title": "Estudo de API",
         "version": "0.0.1"
     });
 });
-app.use("/", route);
+
+const create = router.post('/', (req, res, next)=>{
+    res.status(200).send(req.body);
+});
+
+const del = router.put('/', (req, res, next)=>{
+    res.status(200).send(req.body);
+});
+
+const put = router.post('/', (req, res, next)=>{
+    res.status(200).send(req.body);
+});
+
+app.use("/", create);
 module.exports = app;
---
+++ src/route.js
@@ -0,0 +1,4 @@
+const apiVersion = "v1";
+const apiName = "v1";
+const apiRouteMain = "v1";
+
