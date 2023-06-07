015f262bbde2c778d1dd1854e3a29c762ad00a5e
Trindade Code <trindadecode@gmail.com>
2023-06-07 02:31:03 +0100

    added route ✅

--- src/route.js
+++ src/route.js
@@ -2,3 +2,16 @@ const apiVersion = "v1";
 const apiName = "v1";
 const apiRouteMain = "v1";
 
+
+const create = router.post('/', (req, res, next)=>{
+    res.status(200).send(req.body);
+});
+
+const del = router.put('/', (req, res, next)=>{
+    res.status(200).send(req.body);
+});
+
+const put = router.post('/', (req, res, next)=>{
+    res.status(200).send(req.body);
+});
+
