bd703557d159a6b7e113c26e655ea2230391c07c
Trindade Code <trindadecode@gmail.com>
2023-06-10 18:40:12 +0100

    added model

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,16 +1,28 @@
-'use strict';
+"use strict";
+const product = require("../models/product");
 
-exports.post = (req, res, nex) =>{
+exports.post = (req, res, nex) => {
+    var product = new product(req.body);
+    product
+        .save()
+        .then((_) => {
+            res.status(201).send({ message: "Produto Cadastrado Com Sucesso" });
+        })
+        .catch((e) => {
+            res.status(404).send({
+                message: "Produto Cadastrado Com Sucesso",
+                data: e,
+            });
+        });
     res.status(201).send(req.body);
 };
-exports.delete = (req, res, nex) =>{
+exports.delete = (req, res, nex) => {
     res.status(200).send(req.body);
 };
-exports.put = (req, res, nex) =>{
+exports.put = (req, res, nex) => {
     const id = req.params.id;
     res.status(200).send({
         id: id,
-        item: req.body
+        item: req.body,
     });
 };
-
