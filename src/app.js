c73ca4b7a4fad15956bd83823163ad33be72a73f
Trindade Code <trindadecode@gmail.com>
2023-06-10 18:48:38 +0100

    added model

--- src/app.js
+++ src/app.js
@@ -1,24 +1,32 @@
 'use strict';
 
+const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-
 // parse application/json
-app.use(bodyParser.json())
-
+app.use(bodyParser.json());
 app.use(function (req, res) {
     res.setHeader('Content-Type', 'text/plain')
-    res.write('you posted:\n')
     res.end(JSON.stringify(req.body, null, 2))
-})
+});
 
-//Loader Router
+
+mongoose.connect("mongodb+srv://trindadecode:tXbNt18b6rRQzjgn@nodestore.q0odbzr.mongodb.net/?retryWrites=true&w=majority");
+
+// ? Loader Models
+const product = require("./models/product");
+
+// ? Loader Router
 const indexRoute = require("./routes/index-route");
 const productRoute = require("./routes/product-route");
+
+
+
+
 
 app.use("/", indexRoute);
 app.use("/products", productRoute);
